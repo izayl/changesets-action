e9736bf887c4d84281bd82290cb5a291b0927ef1
izayl <izayl@163.com>
2025-02-28 15:26:17 +0800

    feat: support multi registry

--- src/index.ts
+++ src/index.ts
@@ -60,8 +60,8 @@ const getOptionalInput = (name: string) => core.getInput(name) || undefined;
         core.info("Found existing user .npmrc file");
         const userNpmrcContent = await fs.readFile(userNpmrcPath, "utf8");
         const authLine = userNpmrcContent.split("\n").find((line) => {
-          // check based on https://github.com/npm/cli/blob/8f8f71e4dd5ee66b3b17888faad5a7bf6c657eed/test/lib/adduser.js#L103-L105
-          return /^\s*\/\/registry\.npmjs\.org\/:[_-]authToken=/i.test(line);
+          // check based on https://docs.npmjs.com/cli/v10/configuring-npm/npmrc#auth-related-configuration
+          return /:[_-](authToken|auth|password)=/i.test(line);
         });
         if (authLine) {
           core.info(
